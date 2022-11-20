68f85d1709b416c2c371c75e0faa078df2917583
Катя Рыжова <kataryzova@Air-Kata>
2022-11-20 17:58:59 +0100

    Task 42

---
+++ Task42/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число 
+// в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.Clear();
+
+Console.WriteLine("Type a number ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string bin = "";
+
+while (num>0)
+{
+    bin = num%2+bin;
+    num /= 2;
+}
+
+Console.WriteLine(bin);
