0e21fe4650e32c6a1c5a720d4afe62b5487b8e1a
Катя Рыжова <kataryzova@Air-Kata>
2022-11-20 18:54:35 +0100

    Task45

---
+++ Task45/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива 
+// с помощью поэлементного копирования
+
+Console.Clear();
+
+Console.WriteLine("Type the size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+
+int [] array = new int[size];
+
+
+for (int i = 0; i < size; i++)
+{
+    array [i] = new Random().Next(0,21);
+    Console.Write(array[i] + " ");
+}
+
+int [] array2 = new int[array.Length];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array2[i] = array[i];
+    
+   Console.Write(array2[i] + " ");
+}
